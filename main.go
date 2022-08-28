aa6d929a1db87ac155386da32190a1f918772e7d
Jason Underhill <junderhill@serif.com>
2022-08-28 20:33:11 +0100

    Initial Commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+	"pong-clock/pongclock"
+
+	"github.com/gdamore/tcell/v2"
+)
+
+func main() {
+	screen, err := tcell.NewScreen()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorBlack)
+	screen.SetStyle(defStyle)
+
+	go pongclock.GameLoop(screen)
+
+	for {
+		switch event := screen.PollEvent().(type) {
+		case *tcell.EventResize:
+			screen.Sync()
+		case *tcell.EventKey:
+			if event.Key() == tcell.KeyEscape || event.Key() == tcell.KeyCtrlC {
+				screen.Fini()
+				os.Exit(0)
+			}
+		}
+	}
+}
